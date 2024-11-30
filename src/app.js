d43515cc586d18b47928ff21da9a314b5e0762e1
Alejandrobv25 <alexbavierasaiz@gmail.com>
2024-11-30 01:18:36 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+  let valores = [
+    "A",
+    "1",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palosRandom = palos[Math.floor(Math.random() * palos.lenght)];
+  let valoresRandom = valores[Math.floor(Math.random() * valores.lenght)];
+
+  let iconoArriba = document.querySelector("#iconoArriba");
+  let iconoAbajo = document.querySelector("#iconoAbajo");
+  let valor = document.querySelector("#valor");
+
+  iconoArriba.innerText = palosRandom;
+  iconoAbajo.innerText = palosRandom;
+  valor.innerText = valoresRandom;
+
+  if (palosRandom === "♥" || palosRandom === "♦") {
+    iconoArriba.style.color = "red";
+    iconoAbajo.style.color = "red";
+  } else {
+    iconoArriba.style.color = "black";
+    iconoAbajo.style.color = "black";
+  }
 };
